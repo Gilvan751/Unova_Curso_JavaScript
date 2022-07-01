0f2851c7747aa509e89e897213df51bdb3117e94
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-30 21:51:41 -0300

    Declaração de tipos primitivos de dados

---
+++ aula_tipos_primitivos_referenciais.js
@@ -0,0 +1,17 @@
+//tipos primitivos
+//tipos de referencias
+//string
+let nome = 'Gilvan';
+//number
+let idade = 51;
+
+//boolean
+let estaaprovado = True;
+
+//undefinid
+let sobrenome;
+ // null
+let cor = null;
+
+
+//tipos de valores
