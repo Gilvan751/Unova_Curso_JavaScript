0383ac2a612454bf09f038fb73e4f8e00e39db76
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-07-01 10:29:08 -0300

    Operadores estrit=amente de igualdades

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ console.log(2*salario - salario);
 console.log(5 * salario / salario); */
 
 // ++, --  operadores de imcremento e decremento
-let idade = 18;
+/* let idade = 18; */
 /* console.log(--idade);
 console.log(idade); */
-console.log(idade--);
-console.log(idade);
+/* console.log(idade--);
+console.log(idade); */
 /* console.log(++idade);
 console.log(idade); */
 /* console.log(idade++);
@@ -22,6 +22,18 @@ console.log(idade); */
 
 
 //tipos de Operadores Atribuição
+// +=, -=, *=, / =
+/* let valorTecladoGamer = 200;
+valorTecladoGamer += valorTecladoGamer;
+console.log(valorTecladoGamer); */
 //tipos de Operadores Comparação
+
+//igualdade estrita
+console.log(1 === 1);
+console.log('1' === 1);
+//igualdade solta
+
+console.log(1 == 1);
+console.log('1' == 1);
 //tipos de Operadores Lógicos
 //tipos de Operadores Bitwase
