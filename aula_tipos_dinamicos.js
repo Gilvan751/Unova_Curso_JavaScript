d15d92556477de34c83258e4499dbfcb666b2db6
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-30 21:59:41 -0300

    typeof para saber o tipo de variavel que esta se trabalhando

---
+++ aula_tipos_dinamicos.js
@@ -0,0 +1,9 @@
+//tipagens dinamicas = são os tipos de variaveis que o proprio java script vai dizer quando el linha de execução
+let nome = 'Gilvan';
+let idade = 51;
+// let verdade = True;
+let sobrenome;
+let valor = null;
+console.log(typeof (nome, valor));
+
+//para saber que tipo de variavel temos utilizamos typeof
