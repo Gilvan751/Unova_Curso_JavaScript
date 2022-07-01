29c92524493536d10cc0e60645e418cf4c3bcede
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-07-01 14:29:51 -0300

    loop do...while

--- script.js
+++ script.js
@@ -22,10 +22,16 @@ for i in range(5):
     }
 } */
 
-//2.while
+/* //2.while
 let i = 5;
 while (i >= 1) {
      if (i % 2 != 0) {
         console.log('Estou aprendendo javascript!', i);        
     } i--;
-}
+} */
+//3.Do....while
+let i = 0;
+do{
+    console.log('Digitando...!', i);
+    i++;
+}while(i < 10)
