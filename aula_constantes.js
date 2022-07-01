101a44fd60fc456ef42a507dd8e8971f44ab203c
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-30 21:45:08 -0300

    variaveis e constantes definicçoes

---
+++ aula_constantes.js
@@ -0,0 +1,5 @@
+// let valorIngressoAdulto = 20;
+// valorIngressoAdulto = 30;
+// console.log(valorIngressoAdulto);
+// const valorIngressoAdulto = 15;
+// console.log(valorIngressoAdulto); os valores de constante não podem ser mudados
---
+++ aula_variaveis.js
@@ -0,0 +1,8 @@
+// let idade = 15;
+// let altura = 1.76
+// console.log(idade, altura);
+
+// let peso = prompt('Digite seu peso: ');
+// let altura = prompt('Digite seu altura: ');
+// let imc = (peso / altura **2);
+// alert(imc);
