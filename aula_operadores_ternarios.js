2792f0598475df2820d04c7d7117ed43e2bbeec9
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-07-01 10:45:05 -0300

    operador logicos e ou não

---
+++ aula_operadores_ternarios.js
@@ -0,0 +1,4 @@
+//opoeradores ternarios e o mais recomendado
+let pontos = 200;
+let tipo = pontos > 100 ? 'Premiun' : 'comun';
+console.log(tipo);
--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-//opoeradores ternarios e o mais recomendado
-let pontos = 200;
-let tipo = pontos > 100 ? 'Premiun' : 'comun';
-console.log(tipo);
+// operador logigo e (&&)
+//retorna true se os dois operandos sao verdadeiro
+/* let maiorDeIdade = true;
+let possuiCarteiraDeTrabalho = true;
+let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho;
+console.log(podeAplicar) */
+/* console.log(true && true);
+console.log(false && true); */
+
+//operador logico ou (||)
+//retorna true se apenas um e verdadeiro
+
+let maiorDeIdade = false;
+let possuiCarteiraDeTrabalho = true;
+let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho;
+console.log(podeAplicar)
+
+// operador not (!)
+let candidoRecusado = !podeAplicar;
+console.log(candidoRecusado);
