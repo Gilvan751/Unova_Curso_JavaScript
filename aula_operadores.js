ef74fdfeee2c2ddbaccc610d1b3fb6f8f1b2758d
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-07-01 10:33:11 -0300

    operadores tipo ternário

---
+++ aula_operadores.js
@@ -0,0 +1,39 @@
+//tipos de Operadores Aritmeticos
+/* let salario = 100;
+// +, - , *, /, **
+console.log(salario * salario);
+console.log(salario ** salario);
+console.log(salario + salario);
+console.log(2*salario - salario);
+console.log(5 * salario / salario); */
+
+// ++, --  operadores de imcremento e decremento
+/* let idade = 18; */
+/* console.log(--idade);
+console.log(idade); */
+/* console.log(idade--);
+console.log(idade); */
+/* console.log(++idade);
+console.log(idade); */
+/* console.log(idade++);
+console.log(idade); */
+
+
+
+
+//tipos de Operadores Atribuição
+// +=, -=, *=, / =
+/* let valorTecladoGamer = 200;
+valorTecladoGamer += valorTecladoGamer;
+console.log(valorTecladoGamer); */
+//tipos de Operadores Comparação
+
+//igualdade estrita
+console.log(1 === 1);
+console.log('1' === 1);
+//igualdade solta
+
+console.log(1 == 1);
+console.log('1' == 1);
+//tipos de Operadores Lógicos
+//tipos de Operadores Bitwase
