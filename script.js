7715c89ed07b1c89c425c2c065d6ed52df70cdd8
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-07 18:06:26 -0300

    Função que calcula a velocidade maxima de um radar para aplicação de motos e perca de pontos na carteira de motorista

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-/* const resultado = fizzBuzz(7);
-console.log(resultado);
 
-function fizzBuzz(entrada) {
-    if (typeof entrada != 'number')
-        return "Não é um número";
-    if (entrada % 3 === 0 && entrada % 5 === 0)
-        return 'FizzBuzz';
-    if (entrada % 3 === 0)
-        return 'Fizz';
-    if (entrada % 5 === 0)
-        return 'Buzz';
-    return entrada;
-    
-}; */
-const divisor = fazerAcontecer(23);
-console.log(divisor);
+verificadorVelocidade(130);
 
-function fazerAcontecer(valor) { 
-    if (typeof valor != 'number')
-        return 'Não é um número, entre com um valor numerico! ';
-    if (valor % 2 === 0 && valor % 3 === 0)
-        return 'FazerAcontecer';
-    if (valor % 2 === 0)
-        return 'Fazer';
-    if (valor % 3 === 0)
-        return 'Acontecer';
-    return valor;
+
+function verificadorVelocidade(velocidade) {
+    const valorVelocidadeMaxima = 70;
+    const KmPorPonto = 5;
+    if (velocidade <= valorVelocidadeMaxima )
+        console.log('Ok');
+    else {
+        const pontos = Math.floor(((velocidade - valorVelocidadeMaxima ) / KmPorPonto)) ;
+        if (pontos >= 12)
+            console.log('Carteira Suspensa');
+        else
+            console.log('Pontos ', pontos);
+    }
     
-};
+    
+}
