f8f776140378d55558e62f667166ba83834a3c8c
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-09 15:03:14 -0300

    Verificação de um numero primo

---
+++ aula_numero_primo.js
@@ -0,0 +1,16 @@
+exibirNumeroPrimos(3);
+
+function exibirNumeroPrimos(limite) {
+    for (let numero = 2; numero <= limite; numero++){
+        if (numeroPrimo(numero)) console.log(numero);
+    }
+}
+
+function numeroPrimo(numero) {
+    for (let divisor = 2; divisor < numero; divisor++){
+        if (numero % divisor === 0) {
+            return false;           
+        }
+    }
+    return true;    
+}
