fe747e2385da8caf15a7817ecbf79c439b621b2f
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-11 13:55:30 -0300

    Construtct functions

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const celular = {
-    marcaCelular: 'Asus',
-    tamanhoTela: {
-        Vertical: 155,
-        horizontal: 75
-    },
-    capacidadeBateria: 5000,
-    liga: function () {
+/* function criarCelular(marcaCelular, tamanhoTela, capacidadeBateria) {
+    return {
+    marcaCelular,
+    tamanhoTela,
+    capacidadeBateria,
+    liga () {
         console.log('Fazendo uma ligação...')
+    }
     }
 }
 
-celular.marcaCelular = 'lg';
-console.log(celular.marcaCelular);
+const celular1 = criarCelular('LG', 5, 6, 6000);
+console.log(celular1); */
+//Quando a chave e o parametro são o mesmo apenas uma serve marcaCelular,
+
+function Celular(marcaCelular, tamanhoTela, capacidadeBateria) {
+    this.marcaCelular = marcaCelular,
+    this.tamanhoTela = tamanhoTela, 
+    this.capacidadeBateria = capacidadeBateria,
+    this.ligar = function () {
+        console.log('Fazendo ligação....');
+    }
+}
+
+const celular = new Celular('Asus', 5.5, 5000);
+console.log(celular);
