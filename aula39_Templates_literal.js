a45b706c826177434a8f90bea1010e44f3ed9f99
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-13 18:56:36 -0300

    Adicionando uma nova mensagem no EMC06 usando aspas invertidas

---
+++ aula39_Templates_literal.js
@@ -0,0 +1,15 @@
+const mensagem = 'Oi essa é a nossa\n primeira mensagem!';
+console.log(mensagem);
+
+const outramensagem = 
+`Essa é uma nova
+mesagem que você
+pode entender`
+console.log(outramensagem);
+
+const email = 
+    `Bom dia
+Temos aqui uma nova mensagem
+que você pode substituir
+qualquer nome aqui dentro.`
+console.log(email);
