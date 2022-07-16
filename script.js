a312e14588f5e42ee50e5973de24f5a088cb55d1
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-16 10:32:03 -0300

    Utilização de funçoes para percorrer um local

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let endereco = {
-    Rua: 'Vitoria maria',
-    Cidade: 'Barra do Garças',
-    CEP:'78600-000' 
-        
+function Endereco(rua, cidade, cep) {
+    this.rua = rua,
+    this.cidade = cidade,
+    this.cep = cep
 };
-function exibirEndereco(endereco){
-    for (let chave in endereco)
-        console.log(chave, endereco[chave]);
-}
-    
-exibirEndereco(endereco);
+const endereco1 = new Endereco('a', 'b', 'c');
+const endereco2 = new Endereco('a', 'b', 'c');
+
+function saoIguais(endereco1, endereco2) {
+    return endereco1.rua === endereco2.rua &&
+        endereco1.cidade === endereco2.cidade &&
+        endereco1.cep === endereco2.cep
+};
+
+console.log(saoIguais(endereco1, endereco2));
+function seTemEnderecoMemoriasIguais(endereco1, endereco2) {
+    return endereco1 === endereco2;
+};
+console.log(seTemEnderecoMemoriasIguais(endereco1, endereco2));
