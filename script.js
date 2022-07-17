b44443bd0f18624b997aa793ab96e1ede78dc1d9
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-17 16:36:47 -0300

    Função de construção

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-function Endereco(rua, cidade, cep) {
-    this.rua = rua,
-    this.cidade = cidade,
-    this.cep = cep
-};
-const endereco1 = new Endereco('a', 'b', 'c');
-const endereco2 = new Endereco('a', 'b', 'c');
 
-function saoIguais(endereco1, endereco2) {
-    return endereco1.rua === endereco2.rua &&
-        endereco1.cidade === endereco2.cidade &&
-        endereco1.cep === endereco2.cep
-};
 
-console.log(saoIguais(endereco1, endereco2));
-function seTemEnderecoMemoriasIguais(endereco1, endereco2) {
-    return endereco1 === endereco2;
-};
-console.log(seTemEnderecoMemoriasIguais(endereco1, endereco2));
+function Postagem(titulo, mensagem, autor) {
+    this.titulo= titulo,
+    this.mensagem= mensagem,
+    this.autor= autor,
+    this.visualizacao= 0,
+    this.comentarios = [],
+    this.estaAoVivo = false
+    
+}
+let postagem = new Postagem('a', 'b', 'c');
+console.log(postagem);
+
