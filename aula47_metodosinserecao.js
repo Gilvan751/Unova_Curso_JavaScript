ac33fca41ebbd596c6d765f1490ecfde4ee2ea7d
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-18 16:44:59 -0300

    Metodos de inserção de elementos em uma array

---
+++ aula47_metodosinserecao.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3];
+//adicionar no inicio elementos em uma array-
+numeros.unshift(0)
+console.log(numeros);
+
+//no meio usamos o usplasch
+numeros.splice(1, 2, 12);
+console.log(numeros);
+// no final push
+numeros.push(15);
+console.log(numeros);
