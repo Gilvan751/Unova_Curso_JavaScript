e5d4eb68f1415013b1bee7b4aaeb8f2a189c0ebc
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-23 18:16:15 -0300

    Metodos de verificar uma array

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
+const array1 = [5, 12, 8, 130, 44];
 
+const found = array1.find(element => element > 10);
 
-function Postagem(titulo, mensagem, autor) {
-    this.titulo= titulo,
-    this.mensagem= mensagem,
-    this.autor= autor,
-    this.visualizacao= 0,
-    this.comentarios = [],
-    this.estaAoVivo = false
-    
-}
-let postagem = new Postagem('a', 'b', 'c');
-console.log(postagem);
+console.log(found);
 
