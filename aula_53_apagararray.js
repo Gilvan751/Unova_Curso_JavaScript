055f20435ee1573229deec108e84251a52bdea05
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-25 18:08:12 -0300

    Como apagar uma arrray definitivamente os elementos

---
+++ aula_53_apagararray.js
@@ -0,0 +1,9 @@
+let nomes = ['Paulo', 'Fernanda', 'Fatima', 'João', 'José', 'Hamilcar'];
+let outrosnomes = nomes;
+// Esse numero retirar a array do inicio shitf()
+//nomes = [];
+//console.log(outrosnomes);
+// essa segunda maneira apaga definitivo o modelo inicial nomes.length = 0
+nomes.length = 0;
+console.log(nomes);
+console.log(outrosnomes);
